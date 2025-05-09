173fa1f5b256360602801af0156016f97e92d064
mrpark219 <mrpark219@gmail.com>
2025-05-09 23:00:35 +0900

    ✨ SimpleJob 추가

---
+++ src/main/java/me/park/springbatchstudy/job/SimpleJobConfiguration.java
@@ -0,0 +1,34 @@
+package me.park.springbatchstudy.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+public class SimpleJobConfiguration {
+
+    @Bean
+    public Job simpleJob(JobRepository jobRepository, Step simpleStep1) {
+        return new JobBuilder("simpleJob", jobRepository)
+                .start(simpleStep1)
+                .build();
+    }
+
+    @Bean
+    public Step simpleStep1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("simpleStep1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("=== Simple Job - Step 1 executed ===");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}
